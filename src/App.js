92d01c5132bdb80972f633148b5f397fb8f13457
atulsanini7900 <atulsanini7900@gmail.com>
2026-02-02 17:07:32 +0530

    added product listing page

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import ProductList from './features/product-list/ProductList';
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <div >
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
